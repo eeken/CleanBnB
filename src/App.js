8ff7a97914cec947713c28db9e265a7e8149b995
Marcus <marcus.a.sandberg@outlook.com>
2020-03-20 09:14:47 +0100

    React-strap and React-router-dom added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LandingPage from './Pages/LandingPage'
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <h1>Clean BnB</h1>
+      <Route exact path="/" component={LandingPage}/>
+      </Router>
     </div>
   );
 }
