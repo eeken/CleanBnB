cb3fddc2c6b5c5d646728fdc1414a29135fbe78a
Marcus <marcus.a.sandberg@outlook.com>
2020-03-20 14:27:07 +0100

    Prepared the form Submitting to handle chosen values

--- src/components/SearchAndFilterStart.js
+++ src/components/SearchAndFilterStart.js
@@ -2,40 +2,63 @@ import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 
 function LandingPage() {
+  const [destination, setDestination] = useState("");
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [numberOfGuests, setNumberOfGuests] = useState("");
+
+  const submitSearch = e => {
+    e.preventDefault();
+    console.log("Destination chosen  :  " + destination);
+    console.log("Check-in date chosen  :  " + checkIn);
+    console.log("Check-out date chosen  :  " + checkOut);
+    console.log("Number of guests chosen  :  " + numberOfGuests);
+  };
+
   return (
     <main className="row justify-content-center align-middle">
       <Form
-        className="col-9 col-lg-4 col-md-4 darkbrown golden"
+        className="col-9 col-lg-4 col-md-6 darkbrown golden"
         style={{
           borderRadius: "5px",
           padding: "30px",
           fontWeight: "bold",
           marginTop: "25%"
         }}
+        onSubmit={submitSearch}
       >
         <p style={{ fontSize: "1.4em" }} align="center">
           Book your unique stay around the world!
         </p>
         <FormGroup align="left">
           <Label for="destination-input">Destination </Label>
-          <Input type="text" id="destination-input" className="mb-4"></Input>
+          <Input
+            type="text"
+            id="destination-input"
+            value={destination}
+            onChange={e => setDestination(e.target.value)}
+          ></Input>
         </FormGroup>
 
         <FormGroup>
           <div className="row justify-content-around">
-            <Label for="destination-input">Check-in </Label>
-            <Label for="destination-input">Check-out </Label>
+            <Label for="destination-input"> Check-in </Label>
+            <Label for="destination-input"> Check-out </Label>
           </div>
           <div className="row justify-content-around">
             <Input
               id="destination-input"
               style={{ width: "40%" }}
               type="date"
+              value={checkIn}
+              onChange={e => setCheckIn(e.target.value)}
             />
             <Input
               id="destination-input"
               style={{ width: "40%" }}
               type="date"
+              value={checkOut}
+              onChange={e => setCheckOut(e.target.value)}
             />
           </div>
         </FormGroup>
@@ -44,14 +67,18 @@ function LandingPage() {
           <Label for="guestSelection" align="left">
             Guests
           </Label>
-          <Input type="select" name="guestSelection" id="guestSelection">
-            <option> </option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            <option>6+</option>
+          <Input
+            type="select"
+            name="guestSelection"
+            id="guestSelection"
+            onChange={e => setNumberOfGuests(e.target.value)}
+          >
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+            <option value={6}>6</option>
           </Input>
         </FormGroup>
 
@@ -68,5 +95,4 @@ function LandingPage() {
     </main>
   );
 }
-
 export default LandingPage;
