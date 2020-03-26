833013af22043eb2f5baccc918d33d9a88fa9613
Ola Wiebe <eeken07@gmail.com>
2020-03-26 12:04:03 +0100

    add residence to backend

--- src/main/java/com/example/demo/entities/Residence.java
+++ src/main/java/com/example/demo/entities/Residence.java
@@ -9,18 +9,16 @@ public class Residence {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private int addressid;
     private int size;
-    private int rooms;
+    private int numberofrooms;
     private int maxguests;
+    private int numberofbedrooms;
+    private int numberofbeds;
+    private int numberofbathrooms;
     private int pricepernight;
-
-    public int getPricepernight() {
-        return pricepernight;
-    }
-
-    public void setPricepernight(int pricepernight) {
-        this.pricepernight = pricepernight;
-    }
+    private int amenityprofileid;
+    private int userid;
 
     public int getId() {
         return id;
@@ -28,6 +26,14 @@ public class Residence {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public int getAddressid() {
+        return addressid;
+    }
+
+    public void setAddressid(int addressid) {
+        this.addressid = addressid;
     }
 
     public int getSize() {
@@ -38,20 +44,68 @@ public class Residence {
         this.size = size;
     }
 
-    public int getRooms() {
-        return rooms;
+    public int getNumberofrooms() {
+        return numberofrooms;
     }
 
-    public void setRooms(int rooms) {
-        this.rooms = rooms;
+    public void setNumberofrooms(int numberofrooms) {
+        this.numberofrooms = numberofrooms;
     }
 
-    public int getMaxGuests() {
+    public int getMaxguests() {
         return maxguests;
     }
 
-    public void setMaxGuests(int maxguests) {
+    public void setMaxguests(int maxguests) {
         this.maxguests = maxguests;
+    }
+
+    public int getNumberofbedrooms() {
+        return numberofbedrooms;
+    }
+
+    public void setNumberofbedrooms(int numberofbedrooms) {
+        this.numberofbedrooms = numberofbedrooms;
+    }
+
+    public int getNumberofbeds() {
+        return numberofbeds;
+    }
+
+    public void setNumberofbeds(int numberofbeds) {
+        this.numberofbeds = numberofbeds;
+    }
+
+    public int getNumberofbathrooms() {
+        return numberofbathrooms;
+    }
+
+    public void setNumberofbathrooms(int numberofbathrooms) {
+        this.numberofbathrooms = numberofbathrooms;
+    }
+
+    public int getPricepernight() {
+        return pricepernight;
+    }
+
+    public void setPricepernight(int pricepernight) {
+        this.pricepernight = pricepernight;
+    }
+
+    public int getAmenityprofileid() {
+        return amenityprofileid;
+    }
+
+    public void setAmenityprofileid(int amenityprofileid) {
+        this.amenityprofileid = amenityprofileid;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
     }
 
     public Residence() {}
