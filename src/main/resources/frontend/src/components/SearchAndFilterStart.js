2c623f5fa2a553b35fb3fd46ef72f1f450aadde5
Marcus Sandberg <marcus.a.sandberg@outlook.com>
2020-04-01 11:28:46 +0200

    Sprint clean

--- src/main/resources/frontend/src/components/SearchAndFilterStart.js
+++ src/main/resources/frontend/src/components/SearchAndFilterStart.js
@@ -1,18 +1,13 @@
 import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { useHistory } from "react-router-dom";
 
 function LandingPage() {
-  const [destination, setDestination] = useState("");
-  const [checkIn, setCheckIn] = useState("");
-  const [checkOut, setCheckOut] = useState("");
-  const [numberOfGuests, setNumberOfGuests] = useState("");
+  const history = useHistory();
 
-  const submitSearch = e => {
+  const submitSearch = async e => {
     e.preventDefault();
-    /*console.log("Destination chosen  :  " + destination);
-    console.log("Check-in date chosen  :  " + checkIn);
-    console.log("Check-out date chosen  :  " + checkOut);
-    console.log("Number of guests chosen  :  " + numberOfGuests);*/
+    await history.push("explore");
   };
 
   return (
@@ -24,64 +19,9 @@ function LandingPage() {
         <h2 className="searchAndFilterFormTitle" align="center">
           Book your unique stay around the world!
         </h2>
-        <FormGroup align="left">
-          <Label for="destination-input" className="mb-1">
-            Destination{" "}
-          </Label>
-          <Input
-            type="text"
-            id="destination-input"
-            value={destination}
-            onChange={e => setDestination(e.target.value)}
-          ></Input>
-        </FormGroup>
-
-        <FormGroup>
-          <div className="row justify-content-around">
-            <Label for="destination-input" className="mb-1">
-              Check-in
-            </Label>
-            <Label for="destination-input" className="mb-1">
-              Check-out
-            </Label>
-          </div>
-          <div className="row dateInputRow">
-            <Input
-              className="col-5 dateInput"
-              id="destination-input"
-              type="date"
-              value={checkIn}
-              onChange={e => setCheckIn(e.target.value)}
-            />
-            <Input
-              className="col-5"
-              id="destination-input"
-              type="date"
-              value={checkOut}
-              onChange={e => setCheckOut(e.target.value)}
-            />
-          </div>
-        </FormGroup>
-
-        <FormGroup>
-          <Label for="guestSelection" align="left" className="mb-1">
-            Guests
-          </Label>
-          <Input
-            type="select"
-            name="guestSelection"
-            id="guestSelection"
-            onChange={e => setNumberOfGuests(e.target.value)}
-          >
-            <option value={1}>1</option>
-            <option value={2}>2</option>
-            <option value={3}>3</option>
-            <option value={4}>4</option>
-            <option value={5}>5</option>
-            <option value={6}>6</option>
-          </Input>
-        </FormGroup>
-        <Button className="searchButton" style={{backgroundColor: '#B9986D'}}>SEARCH</Button>
+        <Button className="searchButton" style={{ backgroundColor: "#B9986D" }}>
+          START BROWSING
+        </Button>
       </Form>
     </div>
   );
