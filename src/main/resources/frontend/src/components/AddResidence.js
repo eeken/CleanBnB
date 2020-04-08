8d28f9ee7a3c6b92eee2da38e563fc0840cea236
Victor <Victortornbom@gmail.com>
2020-04-08 15:23:32 +0200

    merge

--- src/main/resources/frontend/src/components/AddResidence.js
+++ src/main/resources/frontend/src/components/AddResidence.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button, Form, FormGroup, Input } from 'reactstrap';
 
 const AddResidence = () => {
   return (
-    <Form className="white">
-      <h5>Have a location for rent?</h5>
+    <Form className="white container golden">
+      <h5 className="mt-4">Have a location for rent?</h5>
       <h5>Add it here!</h5>
       <h5>ABOUT THE RESIDENCE</h5>
-      <FormGroup className="container ">
+      <FormGroup className="container">
         <div className=" row dateInputRow justify-content-space-around align-items-center">
           <Input
             className="col-11 dateInput "
@@ -47,69 +47,82 @@ const AddResidence = () => {
           />
         </div>
       </FormGroup>
-      <FormGroup>
+      <FormGroup className="container">
         <h5>RESIDENCE AMENITIES</h5>
-        <div>
-          <div className="text-left">
-            <Input className="col-5 " type="checkbox" />
-            Balcony
-          </div>
-          <div className="text-right">
-            <Input className="col-5  text-right" type="checkbox" />
-            Swimming Pool
-          </div>
+        <div className="row justify-content-center">
+            <span className="col-5 text-left mb-3 mt-3" style={testStylingBox}>
+            <Input type="checkbox" /> Balcony
+            </span>
+            <span className="col-5 text-left mb-3 mt-3" style={testStylingBox}>
+            <Input type="checkbox" /> Swimming Pool
+            </span>
+            <span className="col-5 text-left mb-3" style={testStylingBox}>
+            <Input type="checkbox" /> Wifi
+            </span>
+            <span className="col-5 text-left mb-3" style={testStylingBox}>
+            <Input type="checkbox" /> Television
+            </span>
+            <span className="col-5 text-left mb-3" style={testStylingBox}>
+            <Input type="checkbox" /> Bathtub
+            </span>
+            <span className="col-5 text-left mb-3" style={testStylingBox}>
+            <Input type="checkbox" /> Washing Machine
+            </span>
+            <span className="col-5 text-left mb-3" style={testStylingBox}>
+            <Input type="checkbox" /> Freezer
+            </span>
+            <span className="col-5 text-left mb-3" style={testStylingBox}>
+            <Input type="checkbox" /> Fridge
+            </span>
+            <span className="col-10 text-left mb-3" style={testStylingBox}>
+            <Input type="checkbox" className="checkBox" /> Dishwasher
+            </span>
         </div>
-        <div>
-          <Input className="col-5" type="checkbox" />
-          WiFi
-          <Input className="col-5" type="checkbox" />
-          TV
-        </div>
-        <div>
-          <Input className="col-5" type="checkbox" />
-          Bathtub
-          <Input className="col-5" type="checkbox" />
-          Washing Machine
-        </div>
-        <div>
-          <Input className="col-5 text-left" type="checkbox" />
-          Fridge
-          <Input className="col-5 text-right" type="checkbox" />
-          Freezer
-        </div>
-        <Input className="col-5" type="checkbox" />
-        Dishwasher
       </FormGroup>
 
-      <FormGroup>
+      <FormGroup className="container">
         <h5>RESIDENCE LOCATION</h5>
-        <Input placeholder="Street" />
-        <div className="dateInputRow">
-          <Input className="col-5 " placeholder="Street Number" />
-          <Input className="col-5" placeholder="Postal Code" />
+        <div className=" row dateInputRow justify-content-space-around align-items-center">
+          <Input className="col-11" style={testStyling} placeholder="Street" />
+
+          <Input
+            className="col-5"
+            placeholder="Street Number"
+            style={testStyling}
+          />
+          <Input
+            className="col-5"
+            placeholder="Postal Code"
+            style={testStyling}
+          />
+
+          <Input style={testStyling} className="col-5 " placeholder="City" />
+          <Input style={testStyling} className="col-5" placeholder="County" />
+
+          <Input
+            className="col-5"
+            id="destination-input"
+            placeholder="Country"
+            style={testStyling}
+          />
         </div>
-        <div className="dateInputRow">
-          <Input className="col-5 " placeholder="City" />
-          <Input className="col-5" placeholder="County" />
-        </div>
-        <Input placeholder="Country " />
       </FormGroup>
       <FormGroup>
         <h5>DESCRIPTION</h5>
         <Input type="textarea" placeholder="Describe your residence here.." />
       </FormGroup>
-      <FormGroup>
+      <FormGroup className="mb-4">
         <h5>AVAILABLE DATES</h5>
         <div className="row dateInputRow">
-          <Input className="col-5" type="date" placeholder="From" />
+          <Input className="col-5" type="date" id="from" placeholder="From" />
           <Input className="col-5" type="date" placeholder="From" />
         </div>
       </FormGroup>
-      <FormGroup>
+      <FormGroup className="container mb-4">
         <h5>RESIDENCE IMAGES</h5>
         <div className="row dateInputRow">
-          <Button className="col-4">ADD IMAGE</Button>
-          <Input className="col-7" placeholder="enter path.." />
+          <Button className="col-5">ADD IMAGE</Button>
+          <Input className="col-6" placeholder="enter path.." />
         </div>
       </FormGroup>
       <FormGroup>
@@ -123,7 +136,16 @@ export default AddResidence
 
 const testStyling = {
   border: "0",
-  borderBottom: "1px #3F392F solid",
+  borderBottom: "0.1px #3F392F solid",
   borderRadius: "0",
   fontStyle: "oblique",
 };
+
+const checkbox = {
+  color: "white"
+}
+
+const testStylingBox = {
+  fontSize: '0.9em',
+  borderRadius: '100%'
+}
