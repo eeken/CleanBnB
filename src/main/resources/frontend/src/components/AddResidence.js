1c283375ec8c92bf4c8d0d4ac23dd25e4f4b5c74
Ola Wiebe <eeken07@gmail.com>
2020-04-09 11:18:07 +0200

    add image upload in addresidence

--- src/main/resources/frontend/src/components/AddResidence.js
+++ src/main/resources/frontend/src/components/AddResidence.js
@@ -1,6 +1,31 @@
 import React, { useState, useContext } from "react";
-import { Button, Form, FormGroup, Input } from "reactstrap";
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import { UserContext } from '../contexts/UserContextProvider'
+
+let images = []
+
+const filesChange = async (fileList) => {
+  // handle file changes
+  const formData = new FormData();
+
+  if (!fileList.length) return;
+
+  // append the files to FormData
+  Array.from(Array(fileList.length).keys()).map((x) => {
+    formData.append("files", fileList[x], fileList[x].name);
+  });
+
+  let response = await fetch("/static/upload", {
+    method: "POST",
+    body: formData,
+  }).catch(console.warn);
+
+  response = await response.json();
+
+  console.log(response);
+
+  images = response;
+};
 
 const AddResidence = () => {
   // RESIDENCE DETAILS
@@ -347,8 +372,14 @@ const AddResidence = () => {
       <FormGroup className="container mb-4">
         <h5>RESIDENCE IMAGES</h5>
         <div className="row dateInputRow">
-          <Button className="col-5">ADD IMAGE</Button>
-          <Input className="col-6" placeholder="enter path.." />
+          <Label for="files">File to upload:</Label>
+          <Input
+            type="file"
+            name="file"
+            id="files"
+            accept=".png,.jpg,.jpeg,.gif,.bmp,.jfif"
+            onChange={(e) => filesChange(e.target.files)}
+          />
         </div>
       </FormGroup>
       <FormGroup>
