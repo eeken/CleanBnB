792d1c11f1efbef91253703b8b521a919f954245
Ola Wiebe <eeken07@gmail.com>
2020-04-09 09:33:02 +0200

    minifix

--- src/main/resources/frontend/src/Pages/MyPage.js
+++ src/main/resources/frontend/src/Pages/MyPage.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Container, Row, Col } from "reactstrap";
 import { UserContext } from '../contexts/UserContextProvider'
 import AddResidence from "../components/AddResidence";
+//import ShowResidence from "../components/ShowResidence";
 
 function MyPage() {
  
@@ -17,13 +18,14 @@ function MyPage() {
     
     <Container  className="themed-container text-center" fluid="sm">
       <Row className="white boxes m-2">
-        <Col className="p-5 golden ">Welcome, {user.firstName} </Col>
+        <Col className="p-5 golden ">Welcome, {user.firstName}! </Col>
       </Row>
       <Row className="golden-bg boxes m-2">
         <Col className="p-3 darkbrowntext">SHOW MY BOOKINGS</Col>
       </Row>
       <Row className="golden-bg boxes m-2">
         <Col className="p-3 darkbrowntext">SHOW MY RESIDENCES</Col>
+        {/* <ShowResidence></ShowResidence> */}
       </Row>
       <Row className="darkbrown boxes m-2">
         <Col className="p-3 golden" >+ ADD A RESIDENCE</Col>
