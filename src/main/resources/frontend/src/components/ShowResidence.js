b12445584297ece2a90c205076cc21ac2f0a6dc9
Paola <paolatosca@outlook.com>
2020-04-16 07:35:01 +0200

    more small design fixes

--- src/main/resources/frontend/src/components/ShowResidence.js
+++ src/main/resources/frontend/src/components/ShowResidence.js
@@ -4,9 +4,6 @@ import { UserContext } from "../contexts/UserContextProvider";
 import { useHistory, useParams } from "react-router-dom";
 
 function ShowResidence() {
-  const { fetchResidence } = useContext(ResidenceContext);
-  const { destination } = useParams();
-  const { numberofguests } = useParams();
   const { user } = useContext(UserContext);
 
   let history = useHistory();
@@ -16,41 +13,47 @@ function ShowResidence() {
       return user.residenceList.map((residence, i) => {
         return (
           <div style={{ backgroundColor: "White" }}>
-          <div
-            key={i}
-            onClick={() => history.push("/details/residence_id=" + residence.id)}
-            className="row text-left residenceCard"
-            style={{ cursor: "pointer", backgroundColor: "White" }}
-          >
-            <div className="col-5 col-lg-2 col-md-3">
-              <img
-                className="cardImage"
-                src={residence.images[0].imagelink}
-                alt="residence-image"
-              />
-            </div>
-            <div className="col-7">
-              <p className="golden mt-0 mb-0 residenceDetailsTitle">
-                {residence.title}
+            <div
+              key={i}
+              onClick={() => history.push("/details/residence_id=" + residence.id)}
+              className="text-left residenceCard"
+              style={{ cursor: "pointer", backgroundColor: "White" }}
+            >
+              <div className="col-5 col-lg-2 col-md-3">
+                <img
+                  className="cardImage"
+                  src={residence.images[0].imagelink}
+                  alt="residence-image"
+                />
+              </div>
+              <div className="col-7 ml-1">
+                <p className="golden mt-0 mb-0 residenceDetailsTitle">
+                  {residence.title}
+                </p>
+                <p className="golden mb-0 residenceDetailsCommonDetails mb-2">
+
+                  {residence.maxguests} guests - {residence.numberofbeds} beds - {residence.rooms}  rooms
               </p>
-              <p className="golden mb-0 residenceDetailsCommonDetails">
-                {residence.address.city}, {residence.address.country}<br></br>
-                {residence.maxguests} guests - {residence.numberofbeds} beds - {residence.rooms}  rooms
-              </p>
-              
-                <p className="golden mb-0 residencePriceDetails">
+              </div>
+              <div className="col-7 ml-1">
+                <p className="golden m-0 residencePriceDetails">
                   ${residence.pricepernight}
                   <span className="residenceDetailsCommonDetails">
                     per night
                 </span>
-              </p>
+                </p>
+              </div>
+              
             </div>
-          </div>
           </div>
         );
       });
     };
-    return <>{list()}</>;
+    return (
+    <div className="mb-3">
+        <>{list()}</>
+    </div>
+    )
   }
   return <div></div>;
 }
